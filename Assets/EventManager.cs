32d09dbb944d23b814cb87b864ea645121cecb1b
JustinY <89873541+jyip808@users.noreply.github.com>
2024-02-29 15:59:19 -1000

    update

--- Assets/EventManager.cs
+++ Assets/EventManager.cs
@@ -26,7 +26,8 @@ public class EventManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 24;
     }
 
     // Update is called once per frame
